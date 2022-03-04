454df483280b40ae661345334bc188430f473374
Furkan Kaya <hello@iamfurkan.com>
2022-03-04 06:30:04 +0300

    first message sent!

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected ' + socket.id);
+  socket.on(
+    "chat message", 
+    (message) => {
+      socket.broadcast.emit("coming message", {id:socket.id, msg: message});
+    }
+  );
 });
 
 server.listen(3000, () => {
